8a7cfd78c6f07a065c3eff14c13d1f23132e25e9
Migueel0 <miggaller@alum.us.es>
2024-05-28 11:06:17 +0200

    project instantation

---
+++ src/example_package/example.py
@@ -0,0 +1,3 @@
+def hello():
+    #This is an example method to understand package structure
+    print("Hello World")
---
+++ src/main.py
@@ -0,0 +1,9 @@
+import example_package.example as example
+
+# main.py
+def main():
+    example.hello() #Call imported module method
+
+
+if __name__ == '__main__':
+    main()
