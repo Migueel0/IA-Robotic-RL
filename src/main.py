03e95767f5f725071bd12ea5abf40cff8e667160
Migueel0 <miggaller@alum.us.es>
2024-05-29 16:04:26 +0200

    FEAT: Implement utils module

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
-import example_package.example as example
+import utils.utils as utils
 
-# main.py
-def main():
-    example.hello() #Call imported module method
+map1 = "./data/map.txt"
+def map1_run():
+    stage,goal = utils.read_stage(map1)
+    utils.view_stage(stage,goal).show()
+    states = utils.calculate_states(stage)
+    utils.view_rewards(stage,states,goal,1000).show()
 
 
 if __name__ == '__main__':
-    main()
+    map1_run()
